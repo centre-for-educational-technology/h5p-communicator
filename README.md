9b722d0c6703e58c3d490805fa8fd79ae726a46a
Karl Aleksander <karl.leoste@gmail.com>
2020-01-28 15:24:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# h5p-communicator
+H5P content type for creating picture audio communicators
---
+++ src/scripts/communicator.js
@@ -0,0 +1,41 @@
+var H5P = H5P || {};
+var $ = H5P.jQuery;
+
+function constructor(options, id) {
+  this.options = options;
+  this.id = id;
+
+  this.playAudio = (url) => {
+    var a = new Audio(url);
+    a.play();
+  };
+}
+
+constructor.prototype.attach = function ($container) {
+  var that = this;
+
+  $container.addClass("h5p-communicator");
+  //$container.append('<p>' + JSON.stringify(this.options) + '</p>');
+
+  var width = this.options.rows[0].length;
+  var height = this.options.rows.length;
+
+  $container.append('<div class="table-display"><table style="width:100%;height:100%;"></table></div>');
+
+  var $table = $container.find('.table-display table');
+
+  this.options.rows.forEach(row => {
+    $table.append('<tr style="height:' + 100 / height +'%;"></tr>');
+
+    var $row = $table.find('tr:last-of-type');
+
+    row.forEach(cell => {
+      $row.append('<td style="width:' + 100 / width + '%;"><img src="' + H5P.getPath(cell.image.path, this.id) + '"></td>');
+
+      var $img = $row.find('td:last-of-type img');
+      $img.click(function () { that.playAudio(H5P.getPath(cell.audio[0].path, that.id)); });
+    });
+  });
+}
+
+H5P.Communicator = constructor;
